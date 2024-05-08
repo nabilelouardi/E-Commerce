4121a6b6f16c70fcd0dd9e9a963cee9cd17ad451
solitairex <solitairelul@gmail.com>
2024-05-08 16:38:09 +0100

    adam fakak was here!

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -43,7 +43,8 @@ namespace E_Commerce.Controllers
 
         {
             //Ayoub benabdoulwahid
-            //Wissal chakir
+            //Wissal chakir 
+            //Adam Fakak
             return a + b;
           
         }
