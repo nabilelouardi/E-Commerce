c0dbb9aab3a74ee60ec80bb7af4119ea035fe188
Soukaina <battahsoukaina@gmail.com>
2024-05-09 16:53:33 +0100

    commit 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -36,7 +36,7 @@ namespace E_Commerce.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
- 
+ //la somme
         public int somme(int a, int b)
         {
 
