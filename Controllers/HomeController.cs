ef914982b967752949e788ad43e0af115d449beb
fruit-bird <github.ru3w9@slmail.me>
2024-05-10 09:01:17 +0100

    fixed somme function

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,54 +40,8 @@ namespace E_Commerce.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
- //la somme
- //..
-
-        /// le code corrige 
-        public int sommeWissal(int a, int b)
-        {
-
-               
-                return a + b;
+        public int somme(int a, int b) {
+            return a + b;
         }
-        public int sommeMahid(int a, int b)
-        {
-            int s = a + b;
-            return s;
-        }
-
- 
-
-
-        public int somme(int a, int b)
-
-        public int sommeDounia(int a, int b)
-
-        {
-
-
-            int a, b, s = 0;
-            s = a + b;
-            return s;
-
-
-
-       
- 
-
-
-            int a, b, s = 0;
-            s = a + b;
-            return s;
-
-
-        }
-        public int sommeEttaoubbali(int a, int b)
-        {
-            int s = a + b;
-            return s;
-        }
-
-
     }
 }
